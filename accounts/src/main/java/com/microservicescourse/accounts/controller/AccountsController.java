f00d034fc0ebc4cce27a4fb87f2614fb1353a084
Felipe Neves <felipe.alneves@outlook.com>
2023-08-30 21:04:38 -0300

    Added configuration for config server

--- accounts/src/main/java/com/microservicescourse/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/microservicescourse/accounts/controller/AccountsController.java
@@ -1,7 +1,11 @@
 package com.microservicescourse.accounts.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microservicescourse.accounts.config.AccountsServiceConfig;
 import com.microservicescourse.accounts.model.Account;
 import com.microservicescourse.accounts.model.Customer;
+import com.microservicescourse.accounts.model.Properties;
 import com.microservicescourse.accounts.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +18,26 @@ public class AccountsController {
 
     private final AccountRepository accountRepository;
 
+    private final AccountsServiceConfig accountsServiceConfig;
+
     @GetMapping("/myAccount")
     public Account getAccountDetails(@RequestBody Customer customer) {
         var account = accountRepository.findByCustomerId(customer.getCustomerId());
 
         return account;
+    }
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        var ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        var properties = Properties.builder()
+                .msg(accountsServiceConfig.getMsg())
+                .buildVersion(accountsServiceConfig.getBuildVersion())
+                .mailDetails(accountsServiceConfig.getMailDetails())
+                .activeBranches(accountsServiceConfig.getActiveBranches())
+                .build();
+
+        var jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
--- loans/src/main/java/com/microservicescourse/loans/controller/LoansController.java
+++ loans/src/main/java/com/microservicescourse/loans/controller/LoansController.java
@@ -1,9 +1,14 @@
 package com.microservicescourse.loans.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microservicescourse.loans.config.LoansServiceConfig;
 import com.microservicescourse.loans.model.Customer;
 import com.microservicescourse.loans.model.Loan;
+import com.microservicescourse.loans.model.Properties;
 import com.microservicescourse.loans.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +20,26 @@ import java.util.List;
 public class LoansController {
 
     private final LoanRepository loanRepository;
+    private final LoansServiceConfig loansServiceConfig;
 
     @PostMapping("/myLoans")
     public List<Loan> getLoanDetails(@RequestBody Customer customer) {
         var loans = loanRepository.findByCustomerIdOrderByStartDateDesc(customer.getCustomerId());
 
         return loans;
+    }
+
+    @GetMapping("/loans/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        var ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        var properties = Properties.builder()
+                .msg(loansServiceConfig.getMsg())
+                .buildVersion(loansServiceConfig.getBuildVersion())
+                .mailDetails(loansServiceConfig.getMailDetails())
+                .activeBranches(loansServiceConfig.getActiveBranches())
+                .build();
+
+        var jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 }
