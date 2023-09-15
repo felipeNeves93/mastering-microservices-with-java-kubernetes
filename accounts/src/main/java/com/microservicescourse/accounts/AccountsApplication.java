438712b107440c9056cca0f6f97295db55afc0e8
Felipe Neves <felipe.alneves@outlook.com>
2023-09-15 10:58:18 -0300

    Added refresh scope configuration

--- accounts/src/main/java/com/microservicescourse/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/microservicescourse/accounts/AccountsApplication.java
@@ -2,12 +2,14 @@ package com.microservicescourse.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class AccountsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
 
 }
